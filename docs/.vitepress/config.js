37108b1613c3d00fac0eb815449323df2c261d55
zmx2321 <zmx2321@163.com>
2023-10-26 15:35:23 +0800

    feat init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,8 @@
 const navConfig = require("./config/navConfig");
 const sidebarConfig = require("./config/sideBarConfig");
 
+// console.log(import.meta.env.VITE_USER_NODE_ENV);
+
 export default {
   title: "zmx的前端日志",
   description: "zmx2321",
@@ -17,7 +19,7 @@ export default {
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     // 增加一个自定义的 favicon(网页标签的图标)
-    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "icon", href: "/vite-blog/favicon.ico" }],
   ],
 
   markdown: {
