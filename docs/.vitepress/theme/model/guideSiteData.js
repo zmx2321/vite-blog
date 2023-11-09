7cf9f811fe6d41f316edc8ae871262c33ff62cb4
zmx2321 <zmx2321@163.com>
2023-11-09 10:42:09 +0800

    feat init

--- docs/.vitepress/theme/model/guideSiteData.js
+++ docs/.vitepress/theme/model/guideSiteData.js
@@ -41,6 +41,35 @@ const siteData = [
       },
     ],
   },
+  {
+    title: "线上的优秀后台管理模板",
+    items: [
+      {
+        icon: "",
+        name: "vue3后台管理模板",
+        desc: "Geeker-Admin 一款基于 Vue3.3、TypeScript、Vite4、Pinia、Element-Plus 开源的后台管理框架，使用目前最新技术栈开发。项目提供强大的 ProTable 组件，在一定程度上提高您的开发效率。另外本项目还封装了一些常用组件、Hooks、指令、动态路由、按钮级别权限控制等功能。",
+        link: `https://admin.spicyboy.cn/#/login`,
+      },
+      {
+        icon: "",
+        name: "react后台管理模板",
+        desc: "🚀🚀🚀 Hooks Admin，基于 React18、React-Router v6、React-Hooks、Redux && Redux-Toolkit、TypeScript、Vite2、Ant-Design 开源的一套后台管理框架。🌈 Redux-Toolkit 版本请切换到 Redux-Toolkit 分支上🚀🚀🚀 Hooks Admin，基于 React18、React-Router v6、React-Hooks、Redux && Redux-Toolkit、TypeScript、Vite2、Ant-Design 开源的一套后台管理框架。",
+        link: `https://hooks.spicyboy.cn/#/login`,
+      },
+      {
+        icon: "",
+        name: "后台管理模板gitee",
+        desc: "大神的gitee代码仓库",
+        link: `https://gitee.com/HalseySpicy`,
+      },
+      {
+        icon: "",
+        name: "后台管理模板github",
+        desc: "大神的github代码仓库",
+        link: `https://github.com/HalseySpicy`,
+      },
+    ],
+  },
 ];
 
 export default siteData;
