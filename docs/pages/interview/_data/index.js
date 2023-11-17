4f2338c176429298683c5b3ad7f68631cafc8c73
zmx2321 <zmx2321@163.com>
2023-11-17 15:30:41 +0800

    feat init

--- docs/pages/interview/_data/index.js
+++ docs/pages/interview/_data/index.js
@@ -5,9 +5,15 @@ const siteData = [
     items: [
       {
         icon: "",
-        name: "腾讯文档",
-        desc: "",
-        link: "https://docs.qq.com/doc/DSHFjdWNzWmFrcW1h",
+        name: "前端面试八股文1",
+        desc: "腾讯文档",
+        link: "https://docs.qq.com/pdf/DSE5wYllaUlFodXJS?",
+      },
+      {
+        icon: "",
+        name: "前端面试八股文2",
+        desc: "腾讯文档",
+        link: "https://docs.qq.com/pdf/DSHVrQUxjaGlKekhh?",
       },
     ],
   },
