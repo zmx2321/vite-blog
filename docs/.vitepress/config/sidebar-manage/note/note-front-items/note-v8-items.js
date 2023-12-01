856afd5202a1d0ead45510c9cc578ad6f6aa95cb
zmx2321 <zmx2321@163.com>
2023-12-01 09:51:05 +0800

    feat init

--- docs/.vitepress/config/sidebar-manage/note/note-front-items/note-v8-items.js
+++ docs/.vitepress/config/sidebar-manage/note/note-front-items/note-v8-items.js
@@ -13,7 +13,7 @@ module.exports = [
       { text: "V8如何提升对象属性访问速度", link: `${baseUrl}/v8-note-4` },
       { text: "函数表达式", link: `${baseUrl}/v8-note-5` },
       { text: "V8是如何实现对象继承的", link: `${baseUrl}/v8-note-6` },
-      { text: "5", link: `${baseUrl}/v8-note-7` },
+      { text: "V8是如何查找变量的", link: `${baseUrl}/v8-note-7` },
       { text: "5", link: `${baseUrl}/v8-note-8` },
       { text: "5", link: `${baseUrl}/v8-note-9` },
       { text: "5", link: `${baseUrl}/v8-note-10` },
