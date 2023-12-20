14a6c610a64580a3321594ab63bafbbad8b8bab3
zmx2321 <zmx2321@163.com>
2023-12-20 11:20:16 +0800

    fix init

--- docs/.vitepress/config/sidebar-manage/note/note-front-items/note-css-items.js
+++ docs/.vitepress/config/sidebar-manage/note/note-front-items/note-css-items.js
@@ -4,5 +4,11 @@
 const baseUrl = "/pages/note/front/css-note";
 
 module.exports = [
-  { text: "css笔记", items: [{ text: "css工作积累", link: `${baseUrl}/css-note-1` }] },
+  {
+    text: "css笔记",
+    items: [
+      { text: "css工作积累", link: `${baseUrl}/css-note-1` },
+      { text: "css高级用法", link: `${baseUrl}/css-note-2` },
+    ],
+  },
 ];
