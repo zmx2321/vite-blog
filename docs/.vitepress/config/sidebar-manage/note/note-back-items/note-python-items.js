55e8b415d51d6231e0ea54290586775322da22ec
zmx2321 <zmx2321@163.com>
2025-03-01 00:05:30 +0800

    fix init

--- docs/.vitepress/config/sidebar-manage/note/note-back-items/note-python-items.js
+++ docs/.vitepress/config/sidebar-manage/note/note-back-items/note-python-items.js
@@ -3,7 +3,24 @@
  */
 const baseUrl = "/pages/note/back/python-note";
 
+// python课程学习笔记
+const pythonCourseNoteConfig = [
+  { text: "python基本概念", link: `${baseUrl}/python-course-note-1` },
+  // { text: "python基础语法", link: `${baseUrl}/python-course-note-2` },
+];
+
+// python工作笔记
+const pythonWorkNoteConfig = [
+  { text: "python工作积累", link: `${baseUrl}/python-note-1` },
+  // { text: "python爬虫", link: `${baseUrl}/python-note-2` },
+];
+
+// python-ai
+const pythonAiNoteConfig = [{ text: "AGI到来", link: `${baseUrl}/python-ai-note-1` }];
+
 module.exports = [
-  { text: "python学习笔记1", link: `${baseUrl}/python-note-1` },
-  { text: "python学习笔记2", link: `${baseUrl}/python-note-2` },
+  { text: "python笔记", items: [] },
+  { text: "python课程学习笔记", items: pythonCourseNoteConfig },
+  { text: "python工作笔记", items: pythonWorkNoteConfig },
+  { text: "pythonAi", items: pythonAiNoteConfig },
 ];
