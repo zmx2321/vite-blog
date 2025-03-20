5e879dd3dd25b2b310c1320a4d5597b7c6a83dd0
zmx2321 <zmx2321@163.com>
2025-03-20 09:59:43 +0800

    fix init

--- docs/.vitepress/config/sidebar-manage/interview/interview-items/interview-advanced-items.js
+++ docs/.vitepress/config/sidebar-manage/interview/interview-items/interview-advanced-items.js
@@ -7,8 +7,11 @@ const baseUrl = "/pages/interview/advanced/interview-advanced";
 module.exports = [
   { text: "原理-事件循环", link: `${baseUrl}-1` },
   { text: "原理-浏览器渲染原理", link: `${baseUrl}-2` },
-  // { text: "实战-歌词滚动效果", link: `${baseUrl}-3` },
-  // { text: "实战-购物车效果", link: `${baseUrl}-4` },
-  { text: "知识-属性描述符", link: `${baseUrl}-5` },
-  { text: "框架-vue从入门到手撕", link: `${baseUrl}-6` },
+  { text: "知识-属性描述符", link: `${baseUrl}-3` },
+  { text: "框架-vue从入门到手撕", link: `${baseUrl}-4` },
+  { text: "对大前端的思考", link: `${baseUrl}-5` },
+  { text: "读与写的深度思考", link: `${baseUrl}-6` },
+  { text: "数组的读与写", link: `${baseUrl}-7` },
+  { text: "数据和函数的内在关系", link: `${baseUrl}-8` },
+  { text: "响应式系统的延伸和扩展", link: `${baseUrl}-9` },
 ];
